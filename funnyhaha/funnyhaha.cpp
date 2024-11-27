779c48abd5d787be026f1fb887826b5ba9aa9912
azulinobh <pcodethehuman@gmail.com>
2024-11-27 00:22:21 -0800

    added a third option: egg

--- funnyhaha/funnyhaha.cpp
+++ funnyhaha/funnyhaha.cpp
@@ -4,6 +4,10 @@
 
 #include <iostream>
 #include <array>
+#include <stdlib.h>
+#include <chrono>
+#include <thread>
+
 
 using namespace std;
 
@@ -13,29 +17,51 @@ void SetColor(int textColor) { cout << "\033[" << textColor << "m"; }
 // Function to reset the console color
 void ResetColor() { cout << "\033[0m"; }
 
-
 int main()
 {
   int colors[6] = {31, 33, 32, 36, 34, 35}; 
         // red yellow green cyan blue magenta
   int choice = 0;
   cout << "Choose an option:" << endl;
-  cout << "\t[1] Normal" << endl;
-  cout << "\t[2] Bright" << endl;
+  cout << "\t[1] Normal colors" << endl;
+  cout << "\t[2] Bright colors" << endl;
+  cout << "\t[3] Egg" << endl;
   cin >> choice;
   switch (choice) {
     case 1:
-      break;
+      break; // normal rainbow
     case 2:
       for (int i = 0; i < 6; i++) {
         colors[i] = colors[i] + 60;
       }
       // brighter colors!!
       break;
+    case 3: // egg
+      for (int i = 0; i < 50; i++) {
+        for (int j = 0; j < i; j++) {
+          cout << endl;
+        }
+        cout << "0" << endl;
+        system("cls");
+      }
+      for (int j = 0; j < 50; j++) {
+        cout << endl;
+      }
+      cout << "\\ /" << endl;
+      system("cls");
+      for (int j = 0; j < 50; j++) {
+        cout << endl;
+      }
+      this_thread::sleep_for(500ms);
+      cout << "____" << endl;
+      return 0;
+    
     default:
       cout << "ебанутый?" << endl;
       return 0;
   }
+
+  // runs only if options 1 or 2 were chosen
   for (;;) {
     for (int i : colors) {
       SetColor(i);
